563d7405d791f185971880052a1f1925188e9310
Kittinutz <knightza94@gmail.com>
2019-04-20 05:43:18 +0700

    fix nugget sill

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 var bodyParser = require('body-parser')
 import math from 'mathjs'
 
-import { Node, range } from './models'
+import { Node, range,variogram,predict } from './models'
 import createRangeTable from './functionHandler/createRangeTable'
 import tranformSemivariance from './functionHandler/tranformSemivariance'
 import createMatrix from './functionHandler/createMatrix'
@@ -15,22 +15,14 @@ let user = [
     { id: 1, name: 'john', surname: 'do', job: 'programmer', salary: 12000 },
 ]
 
-app.get('/', async (req, res) => {
-    // Node.findAll().then(nodeData=>res.send(nodeData))//  1. then
-
+app.get('/getallnode', async (req, res) => {
     const nodeData = await Node.findAll()
     res.send(nodeData)
-    // const nodeData =  await Node.findOne()
-    //  res.send(nodeData)
 })
 
-app.get('/user', (req, res) => {
-    res.send(user)
-})
 
 app.post('/create/node/array', async (req, res) => {
-
-    const { example = [] } = req.body //array
+    const { example = [] } = req.body 
     try {
         example.map(async data => await Node.create(data))
         res.send({ status: 'success' })
@@ -40,29 +32,27 @@ app.post('/create/node/array', async (req, res) => {
     }
 })
 
-app.post('/node/create', async (req, res) => {
-
+app.post('/node/create-with-attitude', async (req, res) => {
     const { latitude, longtitude, attitude } = req.body
-
     const newNode = {
         latitude,
         longtitude,
         attitude,
     }
-    // await Node.create(newNode) //create node  // TODO HERE
-    const nodeDatas = await Node.findAll()// query nodes 30 record
+    await Node.create(newNode)
+    const nodeDatas = await Node.findAll()
     const tranformdata = createRangeTable(nodeDatas)
-    tranformdata.map(async ({ id, distance }) => {
-        distance.map(async (rangeValue, index) => {
-            range.findOrCreate({ where: { nodeId: id, rangeFromNode: index + 1 }, defaults: { range: rangeValue } })
-                .then(([range, create]) => {
-                    console.log(range.get({
-                        plain: true
-                    }))
-                    console.log(create)
-                })
-        })
-    })
+    // tranformdata.map(async ({ id, distance }) => {
+    //     distance.map(async (rangeValue, index) => {
+    //         range.findOrCreate({ where: { nodeId: id, rangeFromNode: index + 1 }, defaults: { range: rangeValue } })
+    //             .then(([range, create]) => {
+    //                 console.log(range.get({
+    //                     plain: true
+    //                 }))
+    //                 console.log(create)
+    //             })
+    //     })
+    // })
     const tranformSemiVarianceData = tranformSemivariance(tranformdata, { NUGGET: 0, SILL: 0.1, RANGE: 300 })
     const tranformMatrix = createMatrix(tranformSemiVarianceData)
     let A = tranformMatrix
@@ -72,36 +62,51 @@ app.post('/node/create', async (req, res) => {
     for (let i = 0; i < tranformSemiVarianceData.length - 1; i += 1) {
         sum += tranformSemiVarianceData[i].attitude * w[i]
     }
-    //res.send(tranformSemiVarianceData)
-    res.send({ status: 'success', value: sum })
-
+    const error =math.sum(math.dotMultiply(tranformSemiVarianceData[tranformSemiVarianceData.length-1].semi,w))
+    const predictData = {
+        varioId:1,
+        zpredict:sum,
+        estimation:Math.abs(attitude-sum),
+        predictError:error
+    }
+    await predict.create(predictData)
+    res.send({ status: 'success', value: sum,error })
 })
-app.put('/user/:id', (req, res) => {
-    let name = req.body.name;
-    let id = +req.params.id
-
-    let userId = user.find((value) => {
-        console.log(value)
-        console.log(typeof value.id)
-        console.log(typeof +req.params.id)
-        return value.id === id
+app.post('/semivariogram/create',async (req,res)=>{
+    const {nugget,sill,range} = req.body
+    await variogram.create({nugget,sill,range})
+    const nodeDatas = await Node.findAll()
+    const getAllVariogram = await variogram.findAll()
+    console.log(getAllVariogram)
+    getAllVariogram.map(async ({ id , nugget,sill,range})=>{
+        const tranformdata = createRangeTable(nodeDatas)
+        const tranformSemiVarianceData = tranformSemivariance(tranformdata, { NUGGET: +nugget, SILL: +sill, RANGE: +range })
+        const tranformMatrix = createMatrix(tranformSemiVarianceData)
+        let A = tranformMatrix
+        let b = tranformSemiVarianceData[tranformSemiVarianceData.length - 1].semi
+        let w = math.multiply(math.inv(A), b)
+        let sum = 0
+        for (let i = 0; i < tranformSemiVarianceData.length - 1; i += 1) {
+            sum += tranformSemiVarianceData[i].attitude * w[i]
+        }
+        const error =math.sum(math.dotMultiply(tranformSemiVarianceData[tranformSemiVarianceData.length-1].semi,w))
+        const {attitude} = nodeDatas[nodeDatas.length-1]
+        predict.findOrCreate({where:{varioId:id}, defaults:{
+            zpredict:sum,
+            estimation:Math.abs(attitude-sum),
+            predictError:error,
+        }}
+        )
     })
-    userId.name = name
-    user[id - 1] = userId;
-    res.send(userId)
+    const getAllPredict = predict.findAll()
+    res.send({data:getAllPredict})    
 })
 
-app.get('/user/:id', (req, res) => {
-    //localhost:3000/1  paramas = 1;
-    let userId = user.find((value) => {
-        console.log(value)
-        console.log(typeof value.id)
-        console.log(typeof +req.params.id)
-        return value.id === +req.params.id
-    })
-    console.log(req.params.id)
-    console.log(userId)
-    res.send(userId)
+app.get('/node/create-with-best-semivariogram',async(req,res)=>{
+   const min = await predict.min('estimation')
+   const predicte = await predict.findOne({where:{estimation:min}})
+   console.log(predicte)
+   res.send({data:predicte})
 })
 
 app.listen(5000, () => {
--- models/predict.js
+++ models/predict.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const predict = sequelize.define('predict', {
+    varioId: DataTypes.INTEGER.UNSIGNED,
     zpredict: DataTypes.DECIMAL,
     estimation: DataTypes.DECIMAL,
     predictError: DataTypes.DECIMAL
