94a9b8a500eca6c3443695c61ac1d71b3673fce2
aplusde <aplusde@gmail.com>
2019-04-25 22:39:15 +0700

    edit index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
 import math from 'mathjs'
+import cors from 'cors'
 
-import { Node, range,variogram,predict } from './models'
+import { Node, range, variogram, predict } from './models'
 
 import createRangeTable from './functionHandler/createRangeTable'
 import tranformSemivariance from './functionHandler/tranformSemivariance'
@@ -11,6 +12,7 @@ import createMatrix from './functionHandler/createMatrix'
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 let user = [
     { id: 1, name: 'john', surname: 'do', job: 'programmer', salary: 12000 },
@@ -23,7 +25,7 @@ app.get('/getallnode', async (req, res) => {
 
 
 app.post('/create/node/array', async (req, res) => {
-    const { example = [] } = req.body 
+    const { example = [] } = req.body
     try {
         example.map(async data => await Node.create(data))
         res.send({ status: 'success' })
@@ -63,23 +65,22 @@ app.post('/node/create-with-attitude', async (req, res) => {
     for (let i = 0; i < tranformSemiVarianceData.length - 1; i += 1) {
         sum += tranformSemiVarianceData[i].attitude * w[i]
     }
-    const error =math.sum(math.dotMultiply(tranformSemiVarianceData[tranformSemiVarianceData.length-1].semi,w))
+    const error = math.sum(math.dotMultiply(tranformSemiVarianceData[tranformSemiVarianceData.length - 1].semi, w))
     const predictData = {
-        varioId:1,
-        zpredict:sum,
-        estimation:Math.abs(attitude-sum),
-        predictError:error
+        varioId: 1,
+        zpredict: sum,
+        estimation: Math.abs(attitude - sum),
+        predictError: error
     }
     await predict.create(predictData)
-    res.send({ status: 'success', value: sum,error })
+    res.send({ status: 'success', value: sum, error })
 })
-app.post('/semivariogram/create',async (req,res)=>{
-    const {nugget,sill,range} = req.body
-    await variogram.create({nugget,sill,range})
+app.post('/semivariogram/create', async (req, res) => {
+    const { nugget, sill, range } = req.body
+    await variogram.create({ nugget, sill, range })
     const nodeDatas = await Node.findAll()
     const getAllVariogram = await variogram.findAll()
-    console.log(getAllVariogram)
-    getAllVariogram.map(async ({ id , nugget,sill,range})=>{
+    getAllVariogram.map(async ({ id, nugget, sill, range }) => {
         const tranformdata = createRangeTable(nodeDatas)
         const tranformSemiVarianceData = tranformSemivariance(tranformdata, { NUGGET: +nugget, SILL: +sill, RANGE: +range })
         const tranformMatrix = createMatrix(tranformSemiVarianceData)
@@ -90,24 +91,26 @@ app.post('/semivariogram/create',async (req,res)=>{
         for (let i = 0; i < tranformSemiVarianceData.length - 1; i += 1) {
             sum += tranformSemiVarianceData[i].attitude * w[i]
         }
-        const error =math.sum(math.dotMultiply(tranformSemiVarianceData[tranformSemiVarianceData.length-1].semi,w))
-        const {attitude} = nodeDatas[nodeDatas.length-1]
-        predict.findOrCreate({where:{varioId:id}, defaults:{
-            zpredict:sum,
-            estimation:Math.abs(attitude-sum),
-            predictError:error,
-        }}
+        const error = math.sum(math.dotMultiply(tranformSemiVarianceData[tranformSemiVarianceData.length - 1].semi, w))
+        const { attitude } = nodeDatas[nodeDatas.length - 1]
+        await predict.findOrCreate({
+            where: { varioId: id }, defaults: {
+                zpredict: sum,
+                estimation: Math.abs(attitude - sum),
+                predictError: error,
+            }
+        }
         )
     })
-    const getAllPredict = predict.findAll()
-    res.send({data:getAllPredict})    
+    const getAllPredict = await predict.findAll()
+    res.send({ data: getAllPredict })
 })
 
-app.get('/node/create-with-best-semivariogram',async(req,res)=>{
-   const min = await predict.min('estimation')
-   const predicte = await predict.findOne({where:{estimation:min}})
-   console.log(predicte)
-   res.send({data:predicte})
+app.get('/node/create-with-best-semivariogram', async (req, res) => {
+    const min = await predict.min('estimation')
+    const predicte = await predict.findOne({ where: { estimation: min } })
+    console.log(predicte)
+    res.send({ data: predicte })
 })
 
 app.listen(5000, () => {
